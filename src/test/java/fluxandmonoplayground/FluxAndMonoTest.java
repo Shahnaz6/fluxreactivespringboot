6b17b6a4fcd0fcad61d10a674e5d29f60b712264
Bhagyashree Gaikwad (contractor) <BH12493@federated.fds>
2019-03-18 16:30:35 -0400

    Added the Flux Error Testing

--- src/test/java/fluxandmonoplayground/FluxAndMonoTest.java
+++ src/test/java/fluxandmonoplayground/FluxAndMonoTest.java
@@ -9,9 +9,21 @@ public class FluxAndMonoTest {
     @Test
     public void Fluxtest(){
       
-       Flux<String> stringFlux=  Flux.just("Spring", "Spring Boot" , "Reactive Spring");
+       Flux<String> stringFlux=  Flux.just("Spring", "Spring Boot" , "Reactive Spring")
+
+/*
+               .concatWith(Flux.error(new RuntimeException("Exception Occured")))
+*/
+               .concatWith(Flux.just("After Error"))
+
+               .log();
        stringFlux
-               .subscribe(System.out::println);
+               .subscribe(System.out::println,
+                       (e) -> System.err.println("Exception is"+e)
+               ,() ->System.out.println("Completed"));
        
     }
+    
+    //Testing Flux 
+    
 }
